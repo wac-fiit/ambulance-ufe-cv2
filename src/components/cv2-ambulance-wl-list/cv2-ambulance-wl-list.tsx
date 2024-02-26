727d73dace33f07bd9ef62fe846e0e4ee41cc9cd
Miroslav Hudak <miroslav.hudak@siemens-healthineers.com>
2024-02-26 15:01:49 +0100

    entry editor and navigation

--- src/components/cv2-ambulance-wl-list/cv2-ambulance-wl-list.tsx
+++ src/components/cv2-ambulance-wl-list/cv2-ambulance-wl-list.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Event, EventEmitter,  Host, h } from '@stencil/core';
 
 @Component({
   tag: 'cv2-ambulance-wl-list',
@@ -6,6 +6,8 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class Cv2AmbulanceWlList {
+
+  @Event({ eventName: "entry-clicked"}) entryClicked: EventEmitter<string>;
 
   waitingPatients: any[];
 
@@ -44,8 +46,8 @@ export class Cv2AmbulanceWlList {
     return (
       <Host>
         <md-list>
-          {this.waitingPatients.map(patient =>
-            <md-list-item>
+          {this.waitingPatients.map((patient, index) =>
+            <md-list-item onClick={ () => this.entryClicked.emit(index.toString())}>
               <div slot="headline">{patient.name}</div>
               <div slot="supporting-text">{"Predpokladaný vstup: " + this.isoDateToLocale(patient.estimatedStart)}</div>
                 <md-icon slot="start">person</md-icon>
