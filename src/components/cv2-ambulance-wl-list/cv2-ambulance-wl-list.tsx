f4b08e28a0623f4ab9853fddb06c9f00f191d281
Miroslav Hudak <miroslav.hudak@siemens-healthineers.com>
2024-03-04 14:35:42 +0100

    Ambulance waiting list CRUD operations

--- src/components/cv2-ambulance-wl-list/cv2-ambulance-wl-list.tsx
+++ src/components/cv2-ambulance-wl-list/cv2-ambulance-wl-list.tsx
@@ -42,8 +42,8 @@ export class Cv2AmbulanceWlList {
           ? <div class="error">{this.errorMessage}</div>
           :
           <md-list>
-            {this.waitingPatients.map((patient, index) =>
-              <md-list-item onClick={() => this.entryClicked.emit(index.toString())}>
+            {this.waitingPatients.map((patient) =>
+              <md-list-item onClick={() => this.entryClicked.emit(patient.id)}>
                 <div slot="headline">{patient.name}</div>
                 <div slot="supporting-text">{"Predpokladaný vstup: " + this.isoDateToLocale(patient.estimatedStart)}</div>
                 <md-icon slot="start">person</md-icon>
@@ -51,6 +51,10 @@ export class Cv2AmbulanceWlList {
             )}
           </md-list>
         }
+        <md-filled-icon-button class="add-button"
+          onclick={() => this.entryClicked.emit("@new")}>
+          <md-icon>add</md-icon>
+        </md-filled-icon-button>
       </Host>
     );
   }
